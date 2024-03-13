0116f9cddd4a4189f4e691c5604a117bab4b0221
bilold1n <babdumutalov436@gmail.com>
2024-03-14 03:20:28 +0500

    vazifa

--- js/app.js
+++ js/app.js
@@ -29,19 +29,41 @@
 // }
 // isempty(obj);
 //3
-let obj = {
-  ism: 20,
-  yosh: 80,
-  adres: "yer sayyorasi",
-};
-function isempty(obj) {
-  let resalt = Object.values(obj);
-  let ushlab = 0;
-  for (let i = 0; i < resalt.length; i++) {
-    if (resalt[i] == String(resalt[i])) {
-    } else {
-    }
+// let obj1 = {
+//   ism: 20,
+//   yosh: 80,
+//   adres: "yer sayyorasi",
+// };
+// function isempty(obj) {
+//   let resalt = Object.values(obj);
+//   let ushlab = 0;
+//   for (let i = 0; i < resalt.length; i++) {
+//     if (typeof resalt[i] == "string") {
+//     } else {
+//       ushlab = ushlab + resalt[i];
+//     }
+//   }
+//   console.log(ushlab);
+// }
+// isempty(obj1);
+//4
+// let arr = [1, 8, 9, 3, 6, 8, 7];
+// function arr1(err1) {
+//   let newarr = [];
+//   err1.forEach((element) => {
+//     newarr.unshift(element);
+//   });
+//   console.log(newarr);
+// }
+// arr1(arr);
+//5
+let arr = +prompt("raqam kiriting");
+function hisob(newarr) {
+  let arr2 = [];
+  if (isNaN(arr) == true) {
+    console.log(arr2);
+  } else {
+    arr2.push(arr2);
   }
-  // console.log(ushlab);
 }
-isempty(obj);
+hisob(arr);
